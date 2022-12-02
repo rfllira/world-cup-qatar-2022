1005e2d181b02108ffef1e81371a645a5620f58f
Rafael Lira <rafaelira347@gmail.com>
2022-12-02 19:12:04 -0300

    FEAT: implementation of logic that lists today's matches

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Header from "./components/header"
 import Body from "./components/body"
-import Footer from "./components/footer"
 
 export default function App() {
   return (
     <div className="App">
       <Header />
       <Body />
-      <Footer />
     </div>
   )
 }
--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,3 +1,4 @@
+//@ts-check
 /* Criar uma aplicação usando React JS que lista os jogos de hoje da copa, e permita seu papite neles.
 
 Requisitos:
@@ -27,10 +28,108 @@ Entrega:
 
 - Ao concluir o teste, responda a esse email, com o link do repositório. E se conseguiu executar ou não como queria...*/
 
+// api: https://raw.githubusercontent.com/openfootball/worldcup.json/master/2022/worldcup.json
+
+import React, { useState } from "react"
+
 export default function Body() {
+  const [date] = useState(new Date())
+  const [clubsApi] = useState(
+    [
+      {
+        "round": "Matchday 13",
+        "group": "Group G",
+        "date": "2022-12-02",
+        "team1": "Serbia",
+        "team2": "Switzerland"
+      },
+      {
+        "round": "Matchday 13",
+        "group": "Group G",
+        "date": "2022-12-02",
+        "team1": "Cameroon",
+        "team2": "Brazil"
+      },
+      {
+        "round": "Matchday 13",
+        "group": "Group H",
+        "date": "2022-12-02",
+        "team1": "Ghana",
+        "team2": "Uruguay"
+      },
+      {
+        "round": "Matchday 13",
+        "group": "Group H",
+        "date": "2022-12-02",
+        "team1": "South Korea",
+        "team2": "Portugal"
+      },
+      {
+        "round": "Matchday 13",
+        "group": "Group G",
+        "date": "2022-12-10",
+        "team1": "Serbia",
+        "team2": "Switzerland"
+      },
+      {
+        "round": "Matchday 13",
+        "group": "Group G",
+        "date": "2022-12-10",
+        "team1": "Cameroon",
+        "team2": "Brazil"
+      },
+      {
+        "round": "Matchday 13",
+        "group": "Group H",
+        "date": "2022-12-10",
+        "team1": "Ghana",
+        "team2": "Uruguay"
+      },
+      {
+        "round": "Matchday 13",
+        "group": "Group H",
+        "date": "2022-12-10",
+        "team1": "South Korea",
+        "team2": "Portugal"
+      },
+    ]
+  )
+
+  const transformDay = () => {
+    const lengthDay = date.getDate().toFixed().length
+
+    if (lengthDay == 1) return `0${date.getDate()}`
+
+    return date.getDate()
+  }
+
+  const filtredForDay = () => {
+    const fullDate = `${date.getFullYear()}-${date.getMonth()+1}-${transformDay()}`
+    const filtered = clubsApi.filter((value) => value.date == fullDate)
+
+    return filtered
+  }
+
+  const getMatchsToday = () => (
+    filtredForDay().map((value, index) => (
+      <div>
+        <p key={index}>
+          <span>{value.team1} x {value.team2} <br /></span>
+          <span>{value.date}<br /></span>
+        </p>
+      </div>
+    ))
+  )
+
   return (
     <div className="Body">
-      <h1>Body</h1>
+      <h1>Hoje</h1>
+      <div>
+        {
+          getMatchsToday()
+        }
+
+      </div>
     </div>
   )
 }
