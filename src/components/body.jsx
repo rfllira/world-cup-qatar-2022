40a038f8f1ac52a6bfd9da64abf283d31d952cde
Rafael Lira <rafaelira347@gmail.com>
2022-12-05 01:00:19 -0300

    FEAT: tweaks

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -33,7 +33,7 @@ export default function Body() {
   return (
     <div>
       <div className='title'>
-        {/* <h1>Horário dos confrontos da Copa do mundo 2022 no Qatar</h1> */}
+        <h1>Horário dos confrontos da Copa do mundo 2022 no Qatar</h1>
       </div>
       <div className="body">
         {
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,8 +2,6 @@ export default function Header() {
   return (
     <div className="Header">
       <img src="./download-logo-fifa-world-cup-qatar-2022-png-e-vector2-removebg-preview.png" alt="" />
-      <h1>Horário dos confrontos da Copa do mundo 2022 no Qatar</h1>
-      <span></span>
     </div>
   )
 }
